974d7926e7cc98db9ac3e1e84b181cf790c17fce
Natálie Sedláková <natalie.sedlakova@betrian.cz>
2025-12-28 22:23:07 +0100

    DATE FIX

--- src/data/2025.ts
+++ src/data/2025.ts
@@ -3,7 +3,7 @@ import { Dataset } from "../utils/types";
 export const dataset2025: Dataset = [
   {
     id: 1,
-    text: "S vaší pomocí tu 31. 1. vznikne pokračování",
+    text: "S vaší pomocí tu 31. 12. vznikne pokračování",
     questions: [],
   },
 ];
